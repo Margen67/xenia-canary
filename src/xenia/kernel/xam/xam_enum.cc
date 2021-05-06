7a4e94f1bd4e36873e36c54ca931f3984690f8da
Gliniak <Gliniak93@gmail.com>
2021-05-06 21:38:10 +0200

    Test Changes to XamEnumerate to provide less janky buffer size

--- src/xenia/kernel/xam/xam_enum.cc
+++ src/xenia/kernel/xam/xam_enum.cc
@@ -39,7 +39,8 @@ uint32_t xeXamEnumerate(uint32_t handle, uint32_t flags, void* buffer,
   if (!e) {
     result = X_ERROR_INVALID_HANDLE;
   } else {
-    size_t actual_buffer_length = buffer_length;
+    size_t actual_buffer_length =
+        buffer_length > e->item_size() ? buffer_length : e->item_size();
     if (buffer_length == e->items_per_enumerate()) {
       actual_buffer_length = e->item_size() * e->items_per_enumerate();
       // Known culprits:
@@ -56,8 +57,6 @@ uint32_t xeXamEnumerate(uint32_t handle, uint32_t flags, void* buffer,
 
     if (e->current_item() >= e->item_count()) {
       result = X_ERROR_NO_MORE_FILES;
-    } else if (actual_buffer_length < e->item_size()) {
-      result = X_ERROR_INSUFFICIENT_BUFFER;
     } else {
       auto item_buffer = static_cast<uint8_t*>(buffer);
       auto max_items = actual_buffer_length / e->item_size();
