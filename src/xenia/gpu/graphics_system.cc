420c53e9c4439420a5208cd32b6c47d1288c8f31
Margen67 <Margen67@users.noreply.github.com>
2021-05-13 05:41:12 -1000

    Add vsync_interval option
    vsync_duration is no longer tied to vsync;
    Old behavior was to set it to 16 if vsync is enabled, and 1 if disabled.
    Default value is 16. (60 frames per second)
    The value represents frametime in milliseconds. For example, for 120FPS, 8 would be specified because 1000/120 = ~8.

--- src/xenia/gpu/graphics_system.cc
+++ src/xenia/gpu/graphics_system.cc
@@ -119,7 +119,7 @@ X_STATUS GraphicsSystem::Setup(cpu::Processor* processor,
   vsync_worker_running_ = true;
   vsync_worker_thread_ = kernel::object_ref<kernel::XHostThread>(
       new kernel::XHostThread(kernel_state_, 128 * 1024, 0, [this]() {
-        uint64_t vsync_duration = cvars::vsync ? 16 : 1;
+        uint64_t vsync_duration = cvars::vsync_interval; //cvars::vsync ? 16 : 1;
         uint64_t last_frame_time = Clock::QueryGuestTickCount();
         while (vsync_worker_running_) {
           uint64_t current_time = Clock::QueryGuestTickCount();
